003eeb972a3149ace3f227253e72103bffaf0a8f
Kevin Gentile <kgentile@protonmail.com>
2018-05-05 20:30:53 -0400

    begin fortnite-tracker api dev

---
+++ api.go
@@ -0,0 +1,9 @@
+package tracker
+
+// GET https://api.fortnitetracker.com/v1/profile/{platform}/{epic-nickname}
+
+//Version contains the current API version for fornite tracker
+const Version = "v1"
+
+//ProfileRoute contains the core route for requesting profile resources
+const ProfileRoute = "https://api.fortnitetracker.com/v1/profile/"
